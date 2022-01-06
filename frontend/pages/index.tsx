594460e4ab0740b4f36c704b81564c86a87fc2b6
Yanchen Ma <yanchenm@gmail.com>
2022-01-06 15:49:18 -0700

    init nextjs + tailwind

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
