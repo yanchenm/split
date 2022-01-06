07013e64d155fa99eef6698c0962b23fa9783847
Yanchen Ma <yanchenm@gmail.com>
2022-01-06 15:57:33 -0700

    add prettier

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
-export default Home
+export default Home;
