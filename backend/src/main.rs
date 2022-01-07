3200ac07f57a8bfd3a8741cc91744f8b841f58c2
Yanchen Ma <yanchenm@gmail.com>
2022-01-07 16:58:25 -0700

    add db pool

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,9 @@
-#[macro_use] extern crate rocket;
+mod db;
+
+#[macro_use]
+extern crate rocket;
+
+use std::env;
 
 #[get("/hello/<name>")]
 fn hello(name: &str) -> String {
@@ -6,6 +11,33 @@ fn hello(name: &str) -> String {
 }
 
 #[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+async fn rocket() -> _ {
+    // Only load .env file if local
+    match env::var("ENV") {
+        Ok(val) if val == "prod" => (),
+        _ => {
+            dotenv::dotenv().ok();
+        }
+    }
+
+    let db_host = env::var("DB_HOST").expect("DB_HOST is not set");
+    let db_port = env::var("DB_PORT")
+        .expect("DB_PORT is not set")
+        .parse::<u16>()
+        .expect("DB_PORT is not a number");
+    let db_name = env::var("DB_NAME").expect("DB_NAME is not set");
+    let db_user = env::var("DB_USER").expect("DB_USER is not set");
+    let db_password = env::var("DB_PASSWORD").expect("DB_PASSWORD is not set");
+
+    let pool = db::initialize(
+        db_host.as_str(),
+        db_port,
+        db_name.as_str(),
+        db_user.as_str(),
+        db_password.as_str(),
+    )
+    .await
+    .expect("Failed to initialize database");
+
+    rocket::build().mount("/", routes![hello]).manage(pool)
 }
