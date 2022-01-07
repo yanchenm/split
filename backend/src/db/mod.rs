fe37c115f7052a50139fdfc120cc132390a297e0
Yanchen Ma <yanchenm@gmail.com>
2022-01-07 16:58:25 -0700

    add db pool

---
+++ backend/src/db/mod.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
+
+const MAX_POOL_SIZE: u32 = 10;
+
+pub async fn initialize(
+    host: &str,
+    port: u16,
+    name: &str,
+    user: &str,
+    password: &str,
+) -> Result<MySqlPool> {
+    let connection_string = format!("mysql://{}:{}@{}:{}/{}", user, password, host, port, name);
+    let pool = MySqlPoolOptions::new()
+        .max_connections(MAX_POOL_SIZE)
+        .connect(&connection_string)
+        .await?;
+    Ok(pool)
+}
