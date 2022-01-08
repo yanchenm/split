3337c0787fee585e786c1042583d64fb672b2af2
Yanchen Ma <yanchenm@gmail.com>
2022-01-08 01:07:53 -0700

    change button colour

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -10,7 +10,7 @@ const Button: React.FC<ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>> = 
   return (
     <button 
       onClick={clickHandler}
-      className="bg-violet-900 hover:bg-blue-500 text-slate-200 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+      className="bg-violet-800 hover:bg-blue-500 text-slate-200 font-semibold hover:text-white py-3 px-4 hover:border-transparent rounded-lg"
     >
       {children}
     </button>
