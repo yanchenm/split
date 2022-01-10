fba12b67e180d7254e773b83e1599432b5940d37
yd-cheng <ydcheng98@gmail.com>
2022-01-09 23:29:56 -0700

    Fixed button import error

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import Button from '../components/Button';
+import Button from '../components/UI/Button';
 import React from 'react';
 import { W3Context } from './_app';
 
