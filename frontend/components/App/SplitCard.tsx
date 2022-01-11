0cbfd86e12a774d8a6a32f97a40199c2cf61cbac
yd-cheng <ydcheng98@gmail.com>
2022-01-10 19:18:52 -0700

    I changed things

--- frontend/components/App/SplitCard.tsx
+++ frontend/components/App/SplitCard.tsx
@@ -4,11 +4,11 @@ import { useRouter } from 'next/router';
 import AppButton from '../UI/AppButton';
 
 type CardProps = {
-  name: string;
-  cost: number;
-  numTxns: number;
-  date: string;
   groupId: string;
+  name: string;
+  currency: string;
+  userBalance: number;
+  lastUpdate: string;
 }
 
 type WrapperProps = {
@@ -18,62 +18,44 @@ type WrapperProps = {
 
 const SplitDetail: React.FC<WrapperProps> = ({ children, svgSrc }) => {
   return (
-    <span className="flex flex-row items-center py-3 pl-8">
+    <span className="flex flex-row items-center py-2 pl-8">
       <img src={svgSrc} className="h-12 w-12" />
-      <div className="font-medium text-2xl mb-1 pl-5">{children}</div>
+      <div className="font-medium text-xl mb-1 pl-5">{children}</div>
     </span>
   )
 }
 
-const SplitCard: React.FC<CardProps> = ({ name, cost, numTxns, date, groupId }) => {
+const SplitCard: React.FC<CardProps> = ({ name, userBalance, currency, lastUpdate, groupId }) => {
 
   const router = useRouter();
-  const [splitId, setSplitId] = useState('');
 
-  // Set groupId on groupId change
-  useEffect(() => {
-    setSplitId(groupId);
-  }, [groupId])
-
-  const editHandler = () => {
+  const viewHandler = () => {
     router.push(`/app/group/${groupId}`)
   }
 
-  const viewHandler = () => {
-    console.log(splitId);
-  }
-
-
   return (
-    <div className="rounded-lg w-64 h-96 bg-slate-900 flex flex-col m-3 text-slate-300">
-      <span className="pt-4 pb-1">
-        <div className="font-bold text-3xl mb-1 text-center">{name}</div>
+    <div className="rounded-lg w-64 h-64 bg-slate-900 flex flex-col m-3 text-slate-300">
+      <span className="pt-2 pb-0">
+        <div className="font-bold text-2xl mb-1 text-center">{name}</div>
       </span>
       
       <hr className="mx-5 my-2 w-5/6 text-slate-600" />
 
-      <SplitDetail svgSrc="/dollar.svg">
-        {cost} USD
-      </SplitDetail>
-
-      <SplitDetail svgSrc="/document_stats.svg">
-        {numTxns}
-        <h3 className="text-base">Transactions</h3>
+      <SplitDetail svgSrc="/green_dollar.svg">
+        <h3 className="text-base text-slate-400">{userBalance < 0 ? "You owe" : "You are owed"}</h3> 
+        {userBalance} {currency}
       </SplitDetail>
 
       <SplitDetail svgSrc="/calendar.svg">
-        {date}
-        <h3 className="text-base">Last Change</h3>
+        <h3 className="text-base text-slate-400">Last Change</h3>
+        {lastUpdate}
       </SplitDetail>
 
       <span className="flex flex-row items-center justify-evenly py-4">
-        <AppButton className="w-40" clickHandler={editHandler}>View Details</AppButton>
+        <AppButton className="w-40" clickHandler={viewHandler}>View Details</AppButton>
       </span>
     </div>
   )
 };
 
 export default SplitCard;
-
-/*
-        */
--- frontend/pages/app/index.tsx
+++ frontend/pages/app/index.tsx
@@ -8,6 +8,44 @@ import Sidebar from '../../components/App/Sidebar';
 
 const Dashboard: NextPage = () => {
 
+  const splitGroups = [
+    {
+      groupId: 'a',
+      name: 'Seattle trip',
+      userBalance: 1,
+      currency: 'USD',
+      lastUpdate: 'Jan 1, 2022'
+    },
+    {
+      groupId: 'b',
+      name: 'Whistler trip',
+      userBalance: -2,
+      currency: 'CAD',
+      lastUpdate: 'Jan 2, 2022'
+    },
+    {
+      groupId: 'c',
+      name: 'Sydney trip',
+      userBalance: 3,
+      currency: 'AUD',
+      lastUpdate: 'Jan 3, 2022'
+    },
+    {
+      groupId: 'd',
+      name: 'Alabama trip',
+      userBalance: -4,
+      currency: 'USD',
+      lastUpdate: 'Jan 4, 2022'
+    },
+    {
+      groupId: 'e',
+      name: 'Quebec trip',
+      userBalance: 5,
+      currency: 'CAD',
+      lastUpdate: 'Jan 5, 2022'
+    }
+  ]
+
   return (
     <div className="min-h-screen flex">
 
@@ -16,15 +54,21 @@ const Dashboard: NextPage = () => {
       {/* Content */}
       <div className="bg-slate-800 flex-1 p-10 text-2xl text-white font-bold h-screen">
         Your Splits
-        {/*<div className="bg-slate-700 mt-10 flex flex-row flex-wrap overflow-y-auto overflow-hidden h-5/6 border-solid border-2 border-slate-500 rounded-xl">*/}
         <div className="mt-10 flex flex-row flex-wrap overflow-y-auto overflow-hidden h-5/6 shadow-xl shadow-slate-900 rounded-xl">
-          <SplitCard groupId="a" name="Vancouver trip" cost={10} numTxns={69} date="Jan 1, 2022" />
-          <SplitCard groupId="b" name="Japan trip" cost={69} numTxns={10} date="Jan 2, 2022" />
-          <SplitCard groupId="c" name="Japan trip" cost={69} numTxns={10} date="Jan 2, 2022" />
-          <SplitCard groupId="d" name="Afghanistan trip" cost={10} numTxns={69} date="Jan 3, 2022" />
-          <SplitCard groupId="e" name="Japan trip" cost={69} numTxns={10} date="Jan 4, 2022" />
-          <SplitCard groupId="d" name="Afghanistan trip" cost={10} numTxns={69} date="Jan 3, 2022" />
-          <SplitCard groupId="e" name="Japan trip" cost={69} numTxns={10} date="Jan 4, 2022" />
+          {
+            splitGroups.map((split) => {
+              return (
+                <SplitCard
+                  key={split.groupId}
+                  groupId={split.groupId}
+                  name={split.name}
+                  currency={split.currency}
+                  userBalance={split.userBalance}
+                  lastUpdate={split.lastUpdate}
+                />
+              )
+            })
+          }
           <NewCard />
         </div>
       </div>
