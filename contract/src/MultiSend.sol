8b21adbbef482a768c566ad3d7e2b21b957ff700
MuchToKnow <justindc99edu@gmail.com>
2022-01-11 02:21:45 -0800

    Adjust transfer logic so user cant take extra eth left in contract

--- contract/src/MultiSend.sol
+++ contract/src/MultiSend.sol
@@ -5,31 +5,22 @@ pragma solidity 0.8.10;
 /// @title MultiSend
 /// @author NotMuchToLearn
 /// @notice Sends eth to multiple parties in one transaction
+/// @dev Thanks to olegabr for the nice transfer logic
 contract MultiSend {
-    // From solmate SafeTransferLib
-    function safeTransferETH(address to, uint256 amount) internal {
-        bool callStatus;
-
-        assembly {
-            // Transfer the ETH and store if it succeeded or not.
-            callStatus := call(gas(), to, amount, 0, 0, 0, 0)
-        }
-
-        require(callStatus, "ETH_TRANSFER_FAILED");
-    }
-
     function multiTransfer(
         address payable[] calldata _addresses,
         uint256[] calldata _amounts
-    ) external payable returns (bool) {
+    ) external payable {
         require(
             _addresses.length == _amounts.length,
             "Number of recipients and amounts do not match"
         );
-        require(_addresses.length != 0, "No recipients to send to");
-        for (uint8 i; i < _addresses.length; i++) {
-            safeTransferETH(_addresses[i], _amounts[i]);
+        require(_addresses.length < 65536);
+        uint256 _value = msg.value;
+        for (uint16 i; i < _addresses.length; i++) {
+            // Rely on overflow reverting the txn
+            _value = _value - _amounts[i];
+            _addresses[i].call{value: _amounts[i]}("");
         }
-        return true;
     }
 }
