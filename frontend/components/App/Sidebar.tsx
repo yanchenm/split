0269973c97bd71b12c3e0af8a116d1d3d6b5d668
Yanchen Ma <yanchenm@gmail.com>
2022-01-11 20:04:51 -0700

    minor ui changes

--- frontend/components/App/Sidebar.tsx
+++ frontend/components/App/Sidebar.tsx
@@ -1,37 +1,24 @@
-import React from 'react';
-import { DarkmodeContext } from '../../pages/_app';
-import { DocumentAddIcon, DocumentSearchIcon, DocumentDuplicateIcon } from '@heroicons/react/outline';
+import { DocumentAddIcon, DocumentDuplicateIcon, DocumentSearchIcon } from '@heroicons/react/outline';
 
+import { DarkmodeContext } from '../../pages/_app';
+import React from 'react';
 import ToggleButton from '../UI/ToggleButton';
 
 type ButtonProps = {
   buttonText: string;
   svgSrc: string;
-}
-
-const SidebarButton: React.FC<ButtonProps> = ({ buttonText, svgSrc }) => {
-  return (
-    <a className="hover:bg-gray-200 dark:hover:bg-slate-800 py-4 px-4 rounded  transition duration-200 flex">
-      <img src={svgSrc} className="h-7 w-7"/>
-      <button className="pl-4 text-left font-medium"> {buttonText} </button>
-    </a>
-  );
-}
+};
 
 const Sidebar: React.FC = () => {
-
   return (
     <DarkmodeContext.Consumer>
       {(darkmodeProps) => {
         return (
-          <div className="bg-gray-200 dark:bg-slate-900 text-neutral-800 dark:text-slate-300 w-64 space-y-6 px-4 py-7 transition duration-200 flex flex-col justify-between">
+          <div className="bg-gray-100 dark:bg-slate-900 text-neutral-800 dark:text-slate-300 w-64 space-y-6 px-4 py-7 transition duration-200 flex flex-col justify-between">
             {/* Logo */}
             <div>
               <a className="flex items-center space-x-2 px-2 mb-10">
-                <span className="text-2xl font-extrabold">
-                  WheresMyMoney
-                  bitch
-                </span>
+                <span className="text-2xl font-extrabold">WheresMyMoney bitch</span>
               </a>
 
               {/* Navbar */}
@@ -48,16 +35,15 @@ const Sidebar: React.FC = () => {
                   <DocumentDuplicateIcon className="h-7 w-7" />
                   <button className="pl-4 text-left font-medium">View Past Groups</button>
                 </div>
-
               </nav>
             </div>
             {/* Dark mode toggle button */}
             <ToggleButton toggleHandler={darkmodeProps.toggleDarkmode} toggleState={darkmodeProps.isDarkmode} />
           </div>
-        )
+        );
       }}
     </DarkmodeContext.Consumer>
-  )
+  );
 };
 
 export default Sidebar;
--- frontend/components/App/SplitCard.tsx
+++ frontend/components/App/SplitCard.tsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import { CurrencyDollarIcon } from '@heroicons/react/solid';
-import { CalendarIcon } from '@heroicons/react/outline';
-
+import { CalendarIcon, CurrencyDollarIcon } from '@heroicons/react/outline';
 
 import AppButton from '../UI/AppButton';
+import React from 'react';
+import { useRouter } from 'next/router';
 
 type CardProps = {
   groupId: string;
@@ -12,12 +10,12 @@ type CardProps = {
   currency: string;
   userBalance: number;
   lastUpdate: string;
-}
+};
 
 type WrapperProps = {
   children: React.ReactNode;
   isDollar: boolean;
-}
+};
 
 const SplitDetail: React.FC<WrapperProps> = ({ children, isDollar }) => {
   return (
@@ -25,37 +23,36 @@ const SplitDetail: React.FC<WrapperProps> = ({ children, isDollar }) => {
       <CurrencyDollarIcon className="h-12 w-12" />
       <div className="font-medium text-xl mb-1 pl-5">{children}</div>
     </span>
-  )
-}
+  );
+};
 
 const SplitCard: React.FC<CardProps> = ({ name, userBalance, currency, lastUpdate, groupId }) => {
-
   const router = useRouter();
 
   const viewHandler = () => {
     router.push({
       pathname: `/app/group/${groupId}`,
-    })
-  }
+    });
+  };
 
   return (
     <div className="bg-gray-100 dark:bg-slate-900 dark:text-slate-300 rounded-lg w-64 h-64 flex flex-col m-3  grow-0">
       <span className="pt-3 pb-2">
         <div className="font-bold text-2xl mb-1 text-center">{name}</div>
       </span>
-      
+
       <hr className="mx-5 py-2 w-5/6" />
 
       <span className="flex flex-row items-center py-2 pt-0 pl-8">
-        <CurrencyDollarIcon className={`h-12 w-12 font-light ${userBalance < 0 ? 'text-red-500' : 'text-green-600'}`} />
+        <CurrencyDollarIcon className={`h-7 w-7 font-light ${userBalance < 0 ? 'text-red-500' : 'text-green-600'}`} />
         <div className="font-medium text-xl mb-1 pl-5">
-          <h3 className="dark:text-slate-400 text-sm">{userBalance < 0 ? "You owe" : "You are owed"}</h3> 
+          <h3 className="dark:text-slate-400 text-sm">{userBalance < 0 ? 'You owe' : 'You are owed'}</h3>
           {userBalance} {currency}
         </div>
       </span>
 
       <span className="flex flex-row items-center py-2 pt-0 pl-8">
-        <CalendarIcon className="h-12 w-12" />
+        <CalendarIcon className="h-7 w-7" />
         <div className="font-medium text-xl mb-1 pl-5">
           <h3 className="dark:text-slate-400 text-sm ">Last Change</h3>
           {lastUpdate}
@@ -63,10 +60,12 @@ const SplitCard: React.FC<CardProps> = ({ name, userBalance, currency, lastUpdat
       </span>
 
       <span className="flex flex-row items-center justify-evenly pt-2 pb-7">
-        <AppButton className="w-40 text-gray-100 font-medium" clickHandler={viewHandler}>View Details</AppButton>
+        <AppButton className="w-40 text-gray-100 font-medium" clickHandler={viewHandler}>
+          View Details
+        </AppButton>
       </span>
     </div>
-  )
+  );
 };
 
 export default SplitCard;
