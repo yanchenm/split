92c88480817cc6075a3200b918a3a2cd89a8ff97
Yanchen Ma <yanchenm@gmail.com>
2022-01-12 03:56:47 -0700

    add share and add txn buttons

--- frontend/pages/app/group/[id].tsx
+++ frontend/pages/app/group/[id].tsx
@@ -1,13 +1,15 @@
 import { DarkmodeContext, W3Context } from '../../_app';
+import { Group, getGroup } from '../../../utils/routes/group';
+import { PlusIcon, ShareIcon } from '@heroicons/react/outline';
+import { Settlement, getSettlementsForGroup } from '../../../utils/routes/settle';
+import { TransactionWithSplits, getTransactionsByGroupWithSplits } from '../../../utils/routes/transaction';
+import { useEffect, useState } from 'react';
+
 import ExpenseList from '../../../components/DetailView/ExpensesList';
 import GroupStats from '../../../components/DetailView/GroupStats';
 import type { NextPage } from 'next/types';
 import Sidebar from '../../../components/App/Sidebar';
 import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import { Group, getGroup } from '../../../utils/routes/group';
-import { Settlement, getSettlementsForGroup } from '../../../utils/routes/settle';
-import { TransactionWithSplits, getTransactionsByGroupWithSplits } from '../../../utils/routes/transaction';
 
 const DetailView: NextPage = () => {
   const router = useRouter();
@@ -54,9 +56,15 @@ const DetailView: NextPage = () => {
                     <Sidebar />
                     <div className="flex flex-row w-full justify-center overflow-y-auto">
                       <div className="flex flex-col space-y-10 w-11/12 items-center">
-                        <h3 className="text-3xl font-semibold mt-9 w-full">{'Test Trip'}</h3>
+                        <div className="flex flex-row justify-between w-full">
+                          <h3 className="text-3xl font-semibold mt-9">{'Test Trip'}</h3>
+                          <div className="flex flex-row mt-9 space-x-3 items-center">
+                            <ShareIcon className="h-8 w-8 hover:text-violet-600 cursor-pointer" />
+                            <PlusIcon className="h-10 w-10 hover:text-violet-600 cursor-pointer" />
+                          </div>
+                        </div>
                         <GroupStats providedWeb3={consumerProps} group={group} settle={settle} />
-                        <ExpenseList group={group} txns={txns} providedWeb3={consumerProps}/>
+                        <ExpenseList group={group} txns={txns} providedWeb3={consumerProps} />
                       </div>
                     </div>
                   </div>
