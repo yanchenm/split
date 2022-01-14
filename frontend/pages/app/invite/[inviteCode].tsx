de178bee605a79f53dc681fb02b6da7ffbd2e32b
Harry Yao <harryyao@asana.com>
2022-01-13 23:24:46 -0800

    add link invalid link

--- frontend/pages/app/invite/[inviteCode].tsx
+++ frontend/pages/app/invite/[inviteCode].tsx
@@ -5,6 +5,7 @@ import ReactLoading from 'react-loading';
 import { acceptInviteToGroup } from '../../../utils/routes/invite';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const AcceptInvite: NextPage = () => {
   const router = useRouter();
@@ -22,11 +23,13 @@ const AcceptInvite: NextPage = () => {
           } else {
             setSuccess(false);
             setStatus(res.data);
+            // router.push('/');
           }
         })
         .catch((err) => {
           setSuccess(false);
           setStatus(err.message);
+          // router.push('/');
         });
     }
   }, [inviteCode]);
@@ -36,7 +39,7 @@ const AcceptInvite: NextPage = () => {
       <Head>
         <title>WheresMyMoney</title>
       </Head>
-      <div className="flex flex-row bg-white dark:bg-slate-800 text-neutral-800 dark:text-slate-400 h-screen w-full">
+      <div className="flex flex-row bg-white dark:bg-slate-800 text-neutral-800 dark:text-slate-400 h-screen w-full font-default">
         <div className="flex flex-row w-full justify-center overflow-y-auto">
           <div className="flex flex-col space-y-10 w-11/12 items-center place-content-center">
             <h3 className="ml-3 text-3xl font-medium text-gray-500 dark:text-slate-400 text-left">
@@ -45,7 +48,15 @@ const AcceptInvite: NextPage = () => {
               ) : success ? (
                 'You have successfully accepted the invite!'
               ) : (
-                "Sorry, the invite didn't work due to: " + status
+                <div>
+                  <div>Sorry, the invite didn't work due to: {status}</div>
+                    <span>
+                      Please register at:  
+                    </span>
+                    <span className="text-violet-600 hover:text-violet-500">
+                      <Link href='/'> wheresmymoney.one</Link>
+                    </span>
+                </div>
               )}
             </h3>
           </div>
